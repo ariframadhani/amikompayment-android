931c27993f12df1224ac0d3801a94d991f0ca1f7
ariframadhani <arifdhn10@gmail.com>
2018-02-04 03:01:29 +0700

    finish layout pembayaran UKM

---
+++ app/src/main/java/com/example/fsd/amikompayment/PembayaranUKMRegister.java
@@ -0,0 +1,13 @@
+package com.example.fsd.amikompayment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PembayaranUKMRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pembayaran_ukmregister);
+    }
+}
---
+++ app/src/main/java/com/example/fsd/amikompayment/UKMRegisterValidation.java
@@ -0,0 +1,13 @@
+package com.example.fsd.amikompayment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UKMRegisterValidation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ukmregister_validation);
+    }
+}
